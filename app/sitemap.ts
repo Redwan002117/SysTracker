4f2d997385d1b5520441f2ea352fa841e93b509d
Redwan Rashid Rico <22303307@iubat.edu>
2026-02-21 14:22:12 +0000

    fix: add export const dynamic='force-static' to manifest/sitemap/robots for output:export compat

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+
 const BASE_URL = 'https://systracker.rico.bd';
 
 export default function sitemap(): MetadataRoute.Sitemap {
